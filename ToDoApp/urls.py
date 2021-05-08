fc79787c7c46bbdc7bfa59a16f3f238a21eb6ff6
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-08 17:47:02 +0600

    corrected url

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import TaskListViews, TaskDetailViews
 
 urlpatterns = [
+    path('', include('authemail.urls')),
     path('api/todo', TaskListViews.as_view(), name='list_todo'),
     path('api/todo/<int:pk>', TaskDetailViews.as_view(), name='todo'),
 ]
