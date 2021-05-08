885767d895ee90d8040d34d237fe429a4146beac
abdulla bektayev <abdulla.bektayev@gmail.com>
2021-05-08 17:51:46 +0600

    changed name for url

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import TaskListViews, TaskDetailViews
 
 urlpatterns = [
-    path('', include('authemail.urls')),
+    path('', include('authemail.urls'), name='authorization'),
     path('api/todo', TaskListViews.as_view(), name='list_todo'),
     path('api/todo/<int:pk>', TaskDetailViews.as_view(), name='todo'),
 ]
